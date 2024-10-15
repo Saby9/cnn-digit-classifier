2ee7ab68a6e31a24bd51718f4b018554331a6c09
Saby9 <144219328+Saby9@users.noreply.github.com>
2024-10-15 23:59:09 +0500

    Load and Preprocess the Data

---
+++ data.py
@@ -0,0 +1,10 @@
+# Load MNIST dataset
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Preprocess the data
+x_train = x_train.reshape((60000, 28, 28, 1)).astype('float32') / 255
+x_test = x_test.reshape((10000, 28, 28, 1)).astype('float32') / 255
+
+# Convert labels to one-hot encoding
+y_train = tf.keras.utils.to_categorical(y_train, 10)
+y_test = tf.keras.utils.to_categorical(y_test, 10)
